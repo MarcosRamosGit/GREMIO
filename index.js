f34c1b13867e5965e0bc8209cea57071c769408b
unknown <vini-master@outlook.com>
2021-04-26 23:03:22 -0300

    quase

---
+++ index.js
@@ -0,0 +1,8 @@
+alert('Bem Vindo(a) à página de nossa chapa, para avançar clique "OK"');
+alert('Acessando esta aba e clicando em "OK" você concorda com todos nossos termos de serviço!');
+Página = "Página oficial da chapa do Grêmio 'TODOS TEMOS VOZ' da escola E.E. Agenor de Campos Mongaguá-SP";
+console.log("Contamos com o seu voto para fazer de nossa escola um lugar mais agradável e cheia de boas novas");
+console.log("Coordenador geral: GUILHERME HENRIQUE DIAS RAMOS");
+console.log("Chapa todos temos voz, AQUI A SUA VOZ É A NOSSA VOZ");
+console.log("Créditos da programação e contato ativo");
+console.log("Programador web Marcos Ramos _ WhatsApp para contato: (13)997345520 precisando de um site publicado na internet por um preço muito em conta? Manda um zap ;)");
